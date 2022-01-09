7f42b2bfb5da97764c0a1c64aca9f66e9e7aab38
Emeraude248 <96231220+Emeraude248@users.noreply.github.com>
2022-01-09 19:23:23 +0100

    2/3 des exos structures

---
+++ freeCodeCamp/Basic Data structures/objets dans objet, chemin pour les modifier.js
@@ -0,0 +1,15 @@
+let userActivity = {
+    id: 23894201352,
+    date: 'January 1, 2017',
+    data: {
+      totalUsers: 51,
+      online: 42
+    }
+  };
+  
+  // Only change code below this line
+  userActivity.data.online = 45
+  //le point pour séparer les éléments du chemin (aucune idées de la validité des termes utilisés ^^)
+  // Only change code above this line
+  
+  console.log(userActivity);
