15044f4496ead627808d6d3227a4e87c00063eca
Emeraude248 <96231220+Emeraude248@users.noreply.github.com>
2022-01-19 14:37:44 +0100

    Update changer le style.js

--- changer le style.js
+++ changer le style.js
@@ -8,3 +8,10 @@ document.querySelector("h4").style.color = "red";
 const h4 = document.querySelector("h4");
 
 h4.style.color = " green";
+
+//on change les tiret en kamelCase:
+
+const questionContainer = document.querySelector(".click-event");
+//dans le doc il y a une div ayant la class click-event
+
+questionContainer.style.borderRadius = "150px";
