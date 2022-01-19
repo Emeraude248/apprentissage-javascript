1e7621d767dda1db93b5288374ca360786f4d7c4
Emeraude248 <96231220+Emeraude248@users.noreply.github.com>
2022-01-19 14:10:28 +0100

    Update changer le style.js

--- changer le style.js
+++ changer le style.js
@@ -2,3 +2,9 @@
 //(voir les variantes pour récup données, QueryS, GetElement...
 
 document.querySelector("h4").style.color = "red";
+
+//meilleur pour selectionné un élément, la constante
+
+const h4 = document.querySelector("h4");
+
+h4.style.color = " green";
