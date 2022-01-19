e284e8197a4a2cf8a8529cb608b237ffcdc712f7
Emeraude248 <96231220+Emeraude248@users.noreply.github.com>
2022-01-19 14:05:42 +0100

    Create changer le style.js

---
+++ changer le style.js
@@ -0,0 +1,4 @@
+//change le style d'un élément avec .style.ceQuOnVeutChanger = "CommentOnVeutLeChanger" après l'avoir identifié 
+//(voir les variantes pour récup données, QueryS, GetElement...
+
+document.querySelector("h4").style.color = "red";
